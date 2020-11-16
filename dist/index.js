97f3e9eb303c3a8baf4a100dff8205d9b11e49d0
dtothefp <dtothefp@gmail.com>
2020-11-16 12:21:53 -0800

    update core

--- dist/index.js
+++ dist/index.js
@@ -43,10 +43,72 @@ module.exports =
 /************************************************************************/
 /******/ ({
 
+/***/ 82:
+/***/ (function(__unusedmodule, exports) {
+
+"use strict";
+
+// We use any as a valid input type
+/* eslint-disable @typescript-eslint/no-explicit-any */
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Sanitizes an input into a string so it can be passed into issueCommand safely
+ * @param input input to sanitize into a string
+ */
+function toCommandValue(input) {
+    if (input === null || input === undefined) {
+        return '';
+    }
+    else if (typeof input === 'string' || input instanceof String) {
+        return input;
+    }
+    return JSON.stringify(input);
+}
+exports.toCommandValue = toCommandValue;
+//# sourceMappingURL=utils.js.map
+
+/***/ }),
+
 /***/ 87:
 /***/ (function(module) {
 
 module.exports = require("os");
+
+/***/ }),
+
+/***/ 102:
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
+
+"use strict";
+
+// For internal use, subject to change.
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// We use any as a valid input type
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const fs = __importStar(__webpack_require__(747));
+const os = __importStar(__webpack_require__(87));
+const utils_1 = __webpack_require__(82);
+function issueCommand(command, message) {
+    const filePath = process.env[`GITHUB_${command}`];
+    if (!filePath) {
+        throw new Error(`Unable to find environment variable for file command ${command}`);
+    }
+    if (!fs.existsSync(filePath)) {
+        throw new Error(`Missing file at path: ${filePath}`);
+    }
+    fs.appendFileSync(filePath, `${utils_1.toCommandValue(message)}${os.EOL}`, {
+        encoding: 'utf8'
+    });
+}
+exports.issueCommand = issueCommand;
+//# sourceMappingURL=file-command.js.map
 
 /***/ }),
 
@@ -94,140 +156,210 @@ run();
 /***/ 149:
 /***/ (function(module, __unusedexports, __webpack_require__) {
 
-let drone = __webpack_require__(561)
+let drone = __webpack_require__(561);
 // platform denotes code hosting provider i.e github, gitlab, bitbucket etc.
 // Had to introduce this variable as there are cases when CI is run on the same platform where code is hosted as those cases need to be handled differently.
 // Default value is github
 let platform = 'github';
-let repo, sha, event, commit_message, pull_request_number, branch, ci, jobUrl, buildUrl
+let repo,
+  sha,
+  event,
+  commit_message,
+  pull_request_target_branch,
+  pull_request_number,
+  branch,
+  ci,
+  jobUrl,
+  buildUrl;
 
 if (process.env.TRAVIS) {
   // Reference: https://docs.travis-ci.com/user/environment-variables
 
-  repo = process.env.TRAVIS_REPO_SLUG
-  sha = process.env.TRAVIS_PULL_REQUEST_SHA || process.env.TRAVIS_COMMIT
-  event = process.env.TRAVIS_EVENT_TYPE
-  commit_message = process.env.TRAVIS_COMMIT_MESSAGE
-  pull_request_number = process.env.TRAVIS_PULL_REQUEST
-  jobUrl = `https://travis-ci.org/${repo}/jobs/${process.env.TRAVIS_JOB_ID}`
-  buildUrl = `https://travis-ci.org/${repo}/builds/${process.env.TRAVIS_JOB_ID}`
+  repo = process.env.TRAVIS_REPO_SLUG;
+  sha = process.env.TRAVIS_PULL_REQUEST_SHA || process.env.TRAVIS_COMMIT;
+  event = process.env.TRAVIS_EVENT_TYPE;
+  commit_message = process.env.TRAVIS_COMMIT_MESSAGE;
+  pull_request_number = process.env.TRAVIS_PULL_REQUEST;
+  jobUrl = `https://travis-ci.org/${repo}/jobs/${process.env.TRAVIS_JOB_ID}`;
+  buildUrl = `https://travis-ci.org/${repo}/builds/${process.env.TRAVIS_JOB_ID}`;
 
   branch =
     process.env.TRAVIS_EVENT_TYPE === 'push'
       ? process.env.TRAVIS_BRANCH
-      : process.env.TRAVIS_PULL_REQUEST_BRANCH
+      : process.env.TRAVIS_PULL_REQUEST_BRANCH;
+  pull_request_target_branch =
+    process.env.TRAVIS_EVENT_TYPE === 'push' 
+      ? ''
+      : process.env.TRAVIS_BRANCH;
 
-  ci = 'travis'
+  ci = 'travis';
 } else if (process.env.CIRCLECI) {
   // Reference: https://circleci.com/docs/1.0/environment-variables
 
-  repo = process.env.CIRCLE_PROJECT_USERNAME + '/' + process.env.CIRCLE_PROJECT_REPONAME
+  repo =
+    process.env.CIRCLE_PROJECT_USERNAME +
+    '/' +
+    process.env.CIRCLE_PROJECT_REPONAME;
 
-  sha = process.env.CIRCLE_SHA1
-  event = 'push'
-  commit_message = '' // circle does not expose commit message
+  sha = process.env.CIRCLE_SHA1;
+  event = 'push';
+  commit_message = ''; // circle does not expose commit message
   if (process.env.CI_PULL_REQUEST) {
-    pull_request_number = process.env.CI_PULL_REQUEST.split('/').pop() // take number from returns url
-    event = 'pull_request'
-  } else pull_request_number = ''
-  branch = process.env.CIRCLE_BRANCH
-  ci = 'circle'
+    pull_request_number = process.env.CI_PULL_REQUEST.split('/').pop(); // take number from returns url
+    event = 'pull_request';
+  } else pull_request_number = '';
+  branch = process.env.CIRCLE_BRANCH;
+  ci = 'circle';
 } else if (process.env.WERCKER) {
   // Reference: https://devcenter.wercker.com/docs/environment-variables/available-env-vars
 
-  repo = process.env.WERCKER_GIT_OWNER + '/' + process.env.WERCKER_GIT_REPOSITORY
+  repo =
+    process.env.WERCKER_GIT_OWNER + '/' + process.env.WERCKER_GIT_REPOSITORY;
 
-  sha = process.env.WERCKER_GIT_COMMIT
-  event = 'push'
-  commit_message = '' // wercker does not expose commit message
-  pull_request_number = '' // wercker does not expose pull request number
-  branch = process.env.WERCKER_GIT_BRANCH
-  ci = 'wercker'
+  sha = process.env.WERCKER_GIT_COMMIT;
+  event = 'push';
+  commit_message = ''; // wercker does not expose commit message
+  pull_request_number = ''; // wercker does not expose pull request number
+  pull_request_target_branch = ''; // wercker does not expose pr target branch
+  branch = process.env.WERCKER_GIT_BRANCH;
+  ci = 'wercker';
 } else if (process.env.DRONE) {
   // Reference: http://readme.drone.io/usage/environment-reference
 
-  repo = process.env.DRONE_REPO || process.env.CI_REPO || drone.getLegacyRepo(process.env)
-  sha = process.env.DRONE_COMMIT || process.env.CI_COMMIT
+  repo =
+    process.env.DRONE_REPO ||
+    process.env.CI_REPO ||
+    drone.getLegacyRepo(process.env);
+  sha = process.env.DRONE_COMMIT || process.env.CI_COMMIT;
   // DRONE_BUILD_EVENT available in drone > v0.5
   // DRONE_EVENT, CI_EVENT available in drone < v0.5
   // no EVENT available in drone < v0.4
-  event = process.env.DRONE_BUILD_EVENT || process.env.DRONE_EVENT || process.env.CI_EVENT || 'push'
-  commit_message = '' // drone does not expose commit message
-  pull_request_number = process.env.DRONE_PULL_REQUEST
-  branch = process.env.DRONE_BRANCH || process.env.CI_BRANCH
-  ci = 'drone'
-} else if (process.env.GITLAB_CI){
+  event =
+    process.env.DRONE_BUILD_EVENT ||
+    process.env.DRONE_EVENT ||
+    process.env.CI_EVENT ||
+    'push';
+  commit_message = ''; // drone does not expose commit message
+  pull_request_number = process.env.DRONE_PULL_REQUEST;
+  pull_request_target_branch = process.env.DRONE_TARGET_BRANCH;
+  branch = process.env.DRONE_BRANCH || process.env.CI_BRANCH;
+  ci = 'drone';
+} else if (process.env.GITLAB_CI) {
   // Reference: https://docs.gitlab.com/ee/ci/variables/predefined_variables.html
   // except buildUrl we get all the other variables for gitlab CI
-  repo = process.env.CI_PROJECT_PATH
-  branch = process.env.CI_COMMIT_REF_NAME
-  commit_message = process.env.CI_COMMIT_MESSAGE
-  pull_request_number = (process.env.CI_MERGE_REQUEST_ID || '') // no pull request numnber in case the CI is run for the branch without a pull request
-  sha=process.env.CI_COMMIT_SHA
-  event = process.env.CI_PIPELINE_SOURCE
-  jobUrl = process.env.CI_JOB_URL
-  platform = 'gitlab'
-  ci = 'gitlab'
+  repo = process.env.CI_PROJECT_PATH;
+  branch = process.env.CI_MERGE_REQUEST_SOURCE_BRANCH_NAME || process.env.CI_COMMIT_REF_NAME;
+  commit_message = process.env.CI_COMMIT_MESSAGE;
+  pull_request_number = process.env.CI_MERGE_REQUEST_ID || ''; // no pull request numnber in case the CI is run for the branch without a pull request
+  pull_request_target_branch = process.env.CI_MERGE_REQUEST_TARGET_BRANCH_NAME || process.env.CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME;
+  sha = process.env.CI_COMMIT_SHA;
+  event = process.env.CI_PIPELINE_SOURCE;
+  jobUrl = process.env.CI_JOB_URL;
+  platform = 'gitlab';
+  ci = 'gitlab';
 } else if (process.env.CI_NAME === 'codeship') {
   // Reference: https://documentation.codeship.com/basic/builds-and-configuration/set-environment-variables/#default-environment-variables
 
-  repo = process.env.CI_REPO_NAME
-  branch = process.env.CI_BRANCH
-  commit_message = process.env.CI_COMMIT_MESSAGE || process.env.CI_MESSAGE
+  repo = process.env.CI_REPO_NAME;
+  branch = process.env.CI_BRANCH;
+  commit_message = process.env.CI_COMMIT_MESSAGE || process.env.CI_MESSAGE;
 
-  event = 'push'
-  pull_request_number = process.env.CI_PR_NUMBER
-  sha=process.env.CI_COMMIT_ID,
-  buildUrl=process.env.CI_BUILD_URL
+  event = 'push';
+  pull_request_number = process.env.CI_PR_NUMBER;
+  pull_request_target_branch = ''; // codeship does not export pr target branch
+  (sha = process.env.CI_COMMIT_ID), (buildUrl = process.env.CI_BUILD_URL);
 
-  ci = 'codeship'
+  ci = 'codeship';
 } else if (process.env.GITHUB_ACTION) {
   // GitHub Actions
   // Reference: https://developer.github.com/actions/creating-github-actions/accessing-the-runtime-environment/
-  
+
   // for pull_request event, GITHUB_REF is of the form refs/pull/<pull_request_number>/merge
   // for push event, GITHUB_REF is of the form refs/heads/<branch>
-  
-  const pull_request_numberORbranch=process.env.GITHUB_REF.split('/')[2];
 
-  repo = process.env.GITHUB_REPOSITORY
-  sha = process.env.GITHUB_SHA
-  event = process.env.GITHUB_EVENT_NAME
-  commit_message = ''
-  pull_request_number = event==='pull_request'? pull_request_numberORbranch:''
+  const pull_request_numberORbranch = process.env.GITHUB_REF.split('/')[2];
+
+  repo = process.env.GITHUB_REPOSITORY;
+  sha = process.env.GITHUB_SHA;
+  event = process.env.GITHUB_EVENT_NAME;
+  commit_message = '';
+  pull_request_number =
+    event === 'pull_request' ? pull_request_numberORbranch : '';
   // GITHUB_HEAD_REF for pull requests. For commits, GITHUB_REF is of the form refs/heads/master, for example
-  branch = event ==='pull_request'? process.env.GITHUB_HEAD_REF : pull_request_numberORbranch
-  ci = 'github_actions'
+  branch =
+    event === 'pull_request'
+      ? process.env.GITHUB_HEAD_REF
+      : pull_request_numberORbranch;
+  // GITHUB_BASE_REF for pull requests, otherwise GITHUB_BASE_REF is empty
+  pull_request_target_branch = process.env.GITHUB_BASE_REF;
+  ci = 'github_actions';
 } else if (process.env.NETLIFY) {
   // Reference: https://www.netlify.com/docs/continuous-deployment/#environment-variables
-  repo = process.env.REPOSITORY_URL.split('@github.com/').pop()
-  event = process.env.PULL_REQUEST?'pull_request':'push'
-  pull_request_number = process.env.PULL_REQUEST?process.env.REVIEW_ID:''
-  sha = process.env.COMMIT_REF
-  branch = process.env.HEAD
-  ci = 'netlify'
+  repo = process.env.REPOSITORY_URL.split('@github.com/').pop();
+  event = process.env.PULL_REQUEST ? 'pull_request' : 'push';
+  pull_request_number = process.env.PULL_REQUEST ? process.env.REVIEW_ID : '';
+  pull_request_target_branch = ''; // netlify does not export pr target branch
+  sha = process.env.COMMIT_REF;
+  branch = process.env.HEAD;
+  ci = 'netlify';
 } else if (process.env.NOW_GITHUB_ORG) {
   // Reference: https://zeit.co/docs/v2/advanced/now-for-github/
-  repo = process.env.NOW_GITHUB_ORG + '/' + process.env.NOW_GITHUB_REPO
-  event = 'push'
-  pull_request_number = ''
-  sha = process.env.NOW_GITHUB_COMMIT_SHA
-  branch = process.env.NOW_GITHUB_COMMIT_REF
-  ci = 'now'
+  repo = process.env.NOW_GITHUB_ORG + '/' + process.env.NOW_GITHUB_REPO;
+  event = 'push';
+  pull_request_number = '';
+  sha = process.env.NOW_GITHUB_COMMIT_SHA;
+  branch = process.env.NOW_GITHUB_COMMIT_REF;
+  ci = 'now';
+} else if(process.env.BUDDY) {
+  repo = process.env.BUDDY_PROJECT_NAME;
+  branch = process.env.BUDDY_EXECUTION_BRANCH;
+  commit_message = process.env.BUDDY_EXECUTION_REVISION_MESSAGE;
+  pull_request_number = process.env.BUDDY_EXECUTION_PULL_REQUEST_NO;
+  pull_request_target_branch = process.env.BUDDY_EXECUTION_PULL_REQUEST_BASE_BRANCH;
+  sha = process.env.BUDDY_EXECUTION_REVISION;
+  event = process.env.BUDDY_EXECUTION_MODE;
+  jobUrl = process.env.BUDDY_EXECUTION_REVISION_URL;
+  platform = 'buddy';
+  ci = 'buddy';
+} else if(process.env.CF_BUILD_URL) {
+  // Reference: https://codefresh.io/docs/docs/codefresh-yaml/variables/#system-provided-variables
+  repo = process.env.CF_REPO_NAME;
+  branch = process.env.CF_BRANCH;
+  commit_message = process.env.CF_COMMIT_MESSAGE;
+  pull_request_number = process.env.CF_PULL_REQUEST_NUMBER;
+  pull_request_target_branch = process.env.CF_PULL_REQUEST_TARGET;
+  sha = process.env.CF_REVISION;
+  event = 'push';
+  buildUrl = process.env.CF_BUILD_URL;
+  platform = 'codefresh';
+  ci = 'codefresh';
 } else if (process.env.CI) {
   // Generic variables for docker images, custom CI builds, etc.
 
-  repo = process.env.CI_REPO_OWNER + '/' + process.env.CI_REPO_NAME
-
-  sha = process.env.CI_COMMIT_SHA
-  event = process.env.CI_EVENT || 'push'
-  commit_message = process.env.CI_COMMIT_MESSAGE
-  pull_request_number = process.env.CI_MERGE_REQUEST_ID
-  branch = process.env.CI_BRANCH
-  ci = 'custom'
+  platform = process.env.CI_PLATFORM;
+  repo = process.env.CI_REPO_OWNER + '/' + process.env.CI_REPO_NAME;
+  sha = process.env.CI_COMMIT_SHA;
+  event = process.env.CI_EVENT || 'push';
+  commit_message = process.env.CI_COMMIT_MESSAGE;
+  pull_request_number = process.env.CI_MERGE_REQUEST_ID;
+  pull_request_target_branch = process.env.CI_TARGET_BRANCH;
+  branch = process.env.CI_BRANCH;
+  ci = process.env.CI;
 }
 
-module.exports = { repo, sha, event, commit_message, branch, pull_request_number, ci, platform, jobUrl, buildUrl }
+module.exports = {
+  repo,
+  sha,
+  event,
+  commit_message,
+  branch,
+  pull_request_number,
+  pull_request_target_branch,
+  ci,
+  platform,
+  jobUrl,
+  buildUrl,
+};
 
 
 /***/ }),
@@ -246,6 +378,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const os = __importStar(__webpack_require__(87));
+const utils_1 = __webpack_require__(82);
 /**
  * Commands
  *
@@ -300,13 +433,13 @@ class Command {
     }
 }
 function escapeData(s) {
-    return (s || '')
+    return utils_1.toCommandValue(s)
         .replace(/%/g, '%25')
         .replace(/\r/g, '%0D')
         .replace(/\n/g, '%0A');
 }
 function escapeProperty(s) {
-    return (s || '')
+    return utils_1.toCommandValue(s)
         .replace(/%/g, '%25')
         .replace(/\r/g, '%0D')
         .replace(/\n/g, '%0A')
@@ -340,6 +473,8 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const command_1 = __webpack_require__(431);
+const file_command_1 = __webpack_require__(102);
+const utils_1 = __webpack_require__(82);
 const os = __importStar(__webpack_require__(87));
 const path = __importStar(__webpack_require__(622));
 /**
@@ -362,11 +497,21 @@ var ExitCode;
 /**
  * Sets env variable for this action and future actions in the job
  * @param name the name of the variable to set
- * @param val the value of the variable
+ * @param val the value of the variable. Non-string values will be converted to a string via JSON.stringify
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function exportVariable(name, val) {
-    process.env[name] = val;
-    command_1.issueCommand('set-env', { name }, val);
+    const convertedVal = utils_1.toCommandValue(val);
+    process.env[name] = convertedVal;
+    const filePath = process.env['GITHUB_ENV'] || '';
+    if (filePath) {
+        const delimiter = '_GitHubActionsFileCommandDelimeter_';
+        const commandValue = `${name}<<${delimiter}${os.EOL}${convertedVal}${os.EOL}${delimiter}`;
+        file_command_1.issueCommand('ENV', commandValue);
+    }
+    else {
+        command_1.issueCommand('set-env', { name }, convertedVal);
+    }
 }
 exports.exportVariable = exportVariable;
 /**
@@ -382,7 +527,13 @@ exports.setSecret = setSecret;
  * @param inputPath
  */
 function addPath(inputPath) {
-    command_1.issueCommand('add-path', {}, inputPath);
+    const filePath = process.env['GITHUB_PATH'] || '';
+    if (filePath) {
+        file_command_1.issueCommand('PATH', inputPath);
+    }
+    else {
+        command_1.issueCommand('add-path', {}, inputPath);
+    }
     process.env['PATH'] = `${inputPath}${path.delimiter}${process.env['PATH']}`;
 }
 exports.addPath = addPath;
@@ -405,12 +556,22 @@ exports.getInput = getInput;
  * Sets the value of an output.
  *
  * @param     name     name of the output to set
- * @param     value    value to store
+ * @param     value    value to store. Non-string values will be converted to a string via JSON.stringify
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function setOutput(name, value) {
     command_1.issueCommand('set-output', { name }, value);
 }
 exports.setOutput = setOutput;
+/**
+ * Enables or disables the echoing of commands into stdout for the rest of the step.
+ * Echoing is disabled by default if ACTIONS_STEP_DEBUG is not set.
+ *
+ */
+function setCommandEcho(enabled) {
+    command_1.issue('echo', enabled ? 'on' : 'off');
+}
+exports.setCommandEcho = setCommandEcho;
 //-----------------------------------------------------------------------
 // Results
 //-----------------------------------------------------------------------
@@ -444,18 +605,18 @@ function debug(message) {
 exports.debug = debug;
 /**
  * Adds an error issue
- * @param message error issue message
+ * @param message error issue message. Errors will be converted to string via toString()
  */
 function error(message) {
-    command_1.issue('error', message);
+    command_1.issue('error', message instanceof Error ? message.toString() : message);
 }
 exports.error = error;
 /**
  * Adds an warning issue
- * @param message warning issue message
+ * @param message warning issue message. Errors will be converted to string via toString()
  */
 function warning(message) {
-    command_1.issue('warning', message);
+    command_1.issue('warning', message instanceof Error ? message.toString() : message);
 }
 exports.warning = warning;
 /**
@@ -513,8 +674,9 @@ exports.group = group;
  * Saves state for current action, the state can only be retrieved by this action's post job execution.
  *
  * @param     name     name of the state to store
- * @param     value    value to store
+ * @param     value    value to store. Non-string values will be converted to a string via JSON.stringify
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function saveState(name, value) {
     command_1.issueCommand('save-state', { name }, value);
 }
@@ -573,6 +735,13 @@ module.exports.getLegacyRepo = getLegacyRepo;
 /***/ (function(module) {
 
 module.exports = require("path");
+
+/***/ }),
+
+/***/ 747:
+/***/ (function(module) {
+
+module.exports = require("fs");
 
 /***/ })
 
